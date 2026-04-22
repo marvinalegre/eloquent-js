f4894e33d457b28605ca67bf6ba3bae5c6d3c6c2
Marvin Alegre <marvinalegredev@gmail.com>
2026-04-22 10:09:40 +0800

    do another from chapter 2

---
+++ chapters/2/chessboard.js
@@ -0,0 +1,26 @@
+const size = 28;
+
+let evenRow = "";
+let oddRow = "";
+for (let i = 0; i < size; i++) {
+  if (i % 2 === 0) {
+    evenRow += " ";
+    oddRow += "#";
+  } else {
+    evenRow += "#";
+    oddRow += " ";
+  }
+}
+evenRow += "\n";
+oddRow += "\n";
+
+let board = "";
+for (let i = 0; i < size; i++) {
+  if (i % 2 === 0) {
+    board += evenRow;
+  } else {
+    board += oddRow;
+  }
+}
+
+console.log(board);
