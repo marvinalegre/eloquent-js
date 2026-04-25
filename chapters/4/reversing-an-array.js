44cdf500e0d6c824169b2e59fcf9b7fa02b9e356
Marvin Alegre <marvinalegredev@gmail.com>
2026-04-25 23:25:48 +0800

    do another from 4

---
+++ chapters/4/reversing-an-array.js
@@ -0,0 +1,26 @@
+function reverseArray(array) {
+  let output = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    output.push(array[i]);
+  }
+  return output;
+}
+
+function reverseArrayInPlace(array) {
+  let temp;
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    temp = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = temp;
+  }
+}
+
+let myArray = ["A", "B", "C"];
+console.log(reverseArray(myArray));
+// → ["C", "B", "A"];
+console.log(myArray);
+// → ["A", "B", "C"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
